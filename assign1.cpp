9b59537c12d0bbc36137d69a4ed2a4a4694ea758
mostafaaa1 <mostafaelesseily@aucegypt.edu>
2021-02-07 12:48:31 -0800

    message

---
+++ assign1.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+void printSolution(int board[N][N])
+{
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = 0; j < N; j++)
+            cout<<board[i][j]<<"  ";
+        cout<<endl;
+    }
+}
+ 
+/* check if a queen can be placed on board[row][col]*/
+bool isSafe(int board[N][N], int row, int col)
+{
+    int i, j;
+    for (i = 0; i < col; i++)
+    {
+        if (board[row][i])
+            return false;
+    }
+    for (i = row, j = col; i >= 0 && j >= 0; i--, j--)
+    {
+        if (board[i][j])
+            return false;
+    }
+ 
+    for (i = row, j = col; j >= 0 && i < N; i++, j--)
+    {
+        if (board[i][j])
+            return false;
+    }
+ 
+    return true;
+}
